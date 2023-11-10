170788728cb25795e7f2de9cd7aa2c9a23fcfc1b
Aldo Jesus Martínez Larios <122809833+AldoJesusMartinezLarios@users.noreply.github.com>
2023-11-09 21:05:02 -0600

    CREATE all of them

---
+++ static/js/editar.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtén el parámetro del correo electrónico de la URL
+    const params = new URLSearchParams(window.location.search);
+    const email = params.get("email");
+
+    // Obtén el formulario y los campos
+    const editContactForm = document.getElementById("edit-contact-form");
+    const emailInput = document.getElementById("email");
+    const nombreInput = document.getElementById("nombre");
+    const telefonoInput = document.getElementById("telefono");
+
+    // Realiza una solicitud para obtener detalles del contacto con el correo electrónico proporcionado
+    fetch(`http://127.0.0.1:8000/contactos/${encodeURIComponent(email)}`)
+        .then(response => response.json())
+        .then(data => {
+            // Llena los campos del formulario con los detalles del contacto
+            emailInput.value = data.email;
+            nombreInput.value = data.nombre;
+            telefonoInput.value = data.telefono;
+        })
+        .catch(error => console.error("Error al obtener detalles del contacto:", error));
+});
+
+function actualizar() {
+    // Obtén los nuevos valores de los campos
+    const nuevoNombre = document.getElementById("nombre").value;
+    const nuevoTelefono = document.getElementById("telefono").value;
+
+    // Obtén el parámetro del correo electrónico de la URL
+    const params = new URLSearchParams(window.location.search);
+    const email = params.get("email");
+
+    // Realiza una solicitud PUT para actualizar el contacto en el backend
+    fetch(`http://127.0.0.1:8000/contactos/${encodeURIComponent(email)}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email,
+            nombre: nuevoNombre,
+            telefono: nuevoTelefono,
+        }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(error => {
+                throw new Error(`Error al actualizar el contacto. Código de estado: ${response.status}. Detalles: ${JSON.stringify(error)}`);
+            });
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Muestra el mensaje de éxito en la página
+        const mensajeElemento = document.getElementById("mensaje");
+        mensajeElemento.innerHTML = `Contacto actualizado con éxito: ${data.email}, ${data.nombre}, ${data.telefono}`;
+
+        // Limpia el mensaje de error si estaba presente
+        const errorMensajeElemento = document.getElementById("error-mensaje");
+        errorMensajeElemento.innerHTML = "";
+
+        // Redirige a la página principal después de una edición exitosa
+        window.location.href = "/";
+    })
+    .catch(error => {
+        // Muestra el mensaje de error en la página
+        const errorMensajeElemento = document.getElementById("error-mensaje");
+        errorMensajeElemento.innerHTML = `Error al actualizar el contacto: ${error.message}`;
+
+        // Limpia el mensaje de éxito si estaba presente
+        const mensajeElemento = document.getElementById("mensaje");
+        mensajeElemento.innerHTML = "";
+    });
+}
--- static/js/ver.js
+++ static/js/ver.js
@@ -1,20 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtén el parámetro de correo electrónico de la URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const email = urlParams.get("email");
+    // Obtén el parámetro del correo electrónico de la URL
+    const params = new URLSearchParams(window.location.search);
+    const email = params.get("email");
 
-    // Puedes usar el valor de 'email' para cargar y mostrar los detalles del contacto
-    // en esta página, por ejemplo, haciendo una solicitud al servidor.
+    // Realiza una solicitud para obtener detalles del contacto con el correo electrónico proporcionado
+    // Puedes utilizar una ruta en tu backend (FastAPI o Flask) para manejar esta solicitud
 
-    // Simulación de detalles de contacto (puedes ajustarlo a tu lógica real)
-    const contactDetails = {
-        email: email,
-        nombre: "Nombre del Contacto",
-        telefono: "555-123-4567"
-    };
-
-    // Muestra los detalles del contacto en la página
-    document.getElementById("email").textContent = contactDetails.email;
-    document.getElementById("nombre").textContent = contactDetails.nombre;
-    document.getElementById("telefono").textContent = contactDetails.telefono;
+    // Ejemplo de solicitud con Fetch API
+    fetch(`http://127.0.0.1:8000/contactos/${encodeURIComponent(email)}`)
+        .then(response => response.json())
+        .then(data => {
+            // Manipula los detalles del contacto y actualiza el contenido en la página
+            const contactDetailsDiv = document.getElementById("contact-details");
+            contactDetailsDiv.innerHTML = `
+                <p>Email: ${data.email}</p>
+                <p>Nombre: ${data.nombre}</p>
+                <p>Teléfono: ${data.telefono}</p>
+                <!-- Agrega más detalles según sea necesario -->
+            `;
+        })
+        .catch(error => console.error("Error al obtener detalles del contacto:", error));
 });
