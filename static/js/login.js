b1ccfb4eb16d8d7ba7a21fe69bcbe481b8369fdb
Brandon Castro Morales <adrimora.0232@gmail.com>
2023-12-13 16:21:50 +0000

    cambios

--- static/js/login.js
+++ static/js/login.js
@@ -2,7 +2,7 @@ async function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    const backendURL = "https://8000-xbrandonmor-autenbacken-b2ajw23ji1b.ws-us106.gitpod.io/"; // Reemplaza con la URL de tu backend
+    const backendURL = "https://8000-xbrandonmor-autenbacken-b2ajw23ji1b.ws-us106.gitpod.io"; // Reemplaza con la URL de tu backend
 
     try {
         const response = await fetch(`${backendURL}/token`, {
@@ -23,10 +23,10 @@ async function login() {
             window.location.href = "/home";
         } else {
             const data = await response.json();
-            document.getElementById("message").innerText = `Login failed. ${data.detail}`;
+            document.getElementById("message").innerText = `Inicio de sesion fallido. ${data.detail}`;
         }
     } catch (error) {
         console.error("Error during login:", error);
-        document.getElementById("message").innerText = "An error occurred during login.";
+        document.getElementById("message").innerText = "Ocurrio un error para iniciar Sesion.";
     }
 }
